951360f3081d3bb676002c0a861f19745be53fb6
Joseph Balikuddembe <baliksjosay@gmail.com>
2020-11-22 21:56:33 +0300

    add token validation with authO

--- src/index.js
+++ src/index.js
@@ -1,27 +1,65 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
-const { buildSchema } = require("graphql");
+const {graphqlHTTP} = require("express-graphql");
+const schema = require('./schema');
+const startDatabase = require('./database');
+const expressPlayground = require('graphql-playground-middleware-express').default;
 
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+// Create a context for holding contextual data
+const context = async () => {
+    const db = await startDatabase();
+  
+    return { db };
+  };
 
-// Provide resolver functions for your schema fields
+const isTokenValid = require('./validate');
+
+// Create a context for holding contextual data
+const context = async req => {
+    const db = await startDatabase();
+    const { authorization: token } = req.headers;
+  
+    return { db, token };
+  };
 const resolvers = {
-  hello: () => "Hello world!"
-};
-
+    events: async (_, context) => {
+      const { db } = await context();
+      return db
+        .collection('events')
+        .find()
+        .toArray();
+    },
+    event: async ({ id }, context) => {
+      const { db } = await context();
+      return db.collection('events').findOne({ id });
+    },
+    editEvent: async ({ id, title, description }, context) => {
+        const { db, token } = await context();
+    
+        const { error } = await isTokenValid(token);
+    
+        if (error) {
+          throw new Error(error);
+        }
+    
+        return db
+          .collection('events')
+          .findOneAndUpdate(
+            { id },
+            { $set: { title, description } },
+            { returnOriginal: false },
+          )
+          .then(resp => resp.value);
+      }
+    };
+  
 const app = express();
-app.use(
-  "/graphql",
-  graphqlHTTP({
+app.use('/graphql', graphqlHTTP(async (req) => ({
     schema,
-    rootValue: resolvers
-  })
+    rootValue: resolvers,
+    context: () => context(req)
+  })),
 );
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
 app.listen(4000);
 
 console.log(`🚀 Server ready at http://localhost:4000/graphql`);
