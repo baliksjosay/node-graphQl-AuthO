205420a3a8415ed90cdc75fe43b68d30cf8cc43e
Joseph Balikuddembe <baliksjosay@gmail.com>
2020-11-24 21:12:36 +0300

    add schema for new events

--- src/schema.js
+++ src/schema.js
@@ -2,24 +2,28 @@ const { buildSchema } = require("graphql");
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
-  type Event {
-    id: ID!
-    title: String!
-    description: String
-    date: String
-    attendants: [Person!]
-  }
+    type Event {
+        id: ID!
+        title: String!
+        description: String
+        date: String
+        attendants: [Person!]
+    }
 
-  type Person {
-    id: ID!
-    name: String!
-    age: Int
-  }
+    type Person {
+        id: ID!
+        name: String!
+        age: Int
+    }
 
-  type Query {
-    events: [Event!]!
-    event(id: Int!): Event!
-  }
+    type Query {
+        events: [Event!]!
+        event(id: Int!): Event!
+    }
+
+    type Mutation {
+      editEvent(id: Int!, title: String!, description: String!): Event!
+    }
 `);
 
 module.exports = schema;
